e9b3270f171f0d821442e26403168a11dac1a7e4
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-09 17:38:08 +0300

    Add checking article condition & display number of posts

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,13 @@
 import Article from './Article';
 
 const Main = ({ articles }) => {
+  if (articles.length === 0) return <h1>No Article found...</h1>;
+
   return (
     <div>
+      <p>
+        <strong>{articles.length} articles</strong> were found
+      </p>
       {articles.map((article, i) => (
         <Article article={article} key={i} />
       ))}
