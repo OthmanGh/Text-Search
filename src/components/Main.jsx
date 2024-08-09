9796ec1c449dc35c255f8b90ec66ca9047409d3a
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-09 17:13:56 +0300

    Render articles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,13 @@
-const Main = () => {
-  return <div>Main Content</div>;
+import Article from './Article';
+
+const Main = ({ articles }) => {
+  return (
+    <div>
+      {articles.map((article, i) => (
+        <Article article={article} key={i} />
+      ))}
+    </div>
+  );
 };
 
 export default Main;
