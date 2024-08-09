e9eb92990f7ec03ea2fe3d21b6226f02c050fbfc
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-09 18:42:33 +0300

    Replace text with highlighted one

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,13 +1,16 @@
-import formatDate from '../utils/helper';
+import { highlightText, formatDate } from '../utils/helper';
 
-const Article = ({ article }) => {
+const Article = ({ article, searchQuery }) => {
+  const highlightedTitle = highlightText(article.title, searchQuery);
+  const highlightedContent = highlightText(article.content, searchQuery);
+
   return (
-    <div className="  border-b-slate-500 border-2 py-4 mb-10 max-w-[50%] flex flex-col gap-5">
-      <h2 className="text-3xl font-bold">{article.title}</h2>
+    <div className="border-b-slate-500 border-2 py-4 mb-10 max-w-[50%] flex flex-col gap-5">
+      <h2 className="text-3xl font-bold">{highlightedTitle}</h2>
       <p>
         <em>{formatDate(article.date)}</em>
       </p>
-      <p className="text-gray-600 text-xl font-semibold">{article.content}</p>
+      <p className="text-gray-600 text-xl font-semibold">{highlightedContent}</p>
     </div>
   );
 };
