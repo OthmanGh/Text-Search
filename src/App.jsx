4222056337099d20a3e4189f17c2a2eb97ca97f0
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-08-09 17:36:48 +0300

    Add search query state for filtering functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+import { articles as dummyArticles } from './constants';
 
 const App = () => {
+  const [articles, setArticles] = useState(dummyArticles);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const searchedArticle =
+    searchQuery.length > 0 ? articles.filter((article) => `${article.title}`.toLowerCase().includes(searchQuery.toLowerCase())) : articles;
+
   return (
     <div>
-      <Header />
-      <Main />
+      <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <Main articles={searchedArticle} />
     </div>
   );
 };
